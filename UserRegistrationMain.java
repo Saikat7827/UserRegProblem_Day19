8912128834d72162efb3426794114729a7f67b62
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-02 00:00:59 +0530

    Added: Ability to Check Last name Weather Valid or not

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -4,16 +4,15 @@ import java.util.Scanner;
 
 public class UserRegistrationMain extends UserDetailsValidation{
     static Scanner scanner = new Scanner(System.in);
-    static String firstName;
+    static String  lastName;
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem");
-        userFirstName();
+        userLastName();
     }
-
-    public static void userFirstName() {
-        System.out.println("Enter your First Name");
-        firstName = scanner.next();
-        UserDetailsValidation.isValidFirstName(firstName);
+    public static void userLastName() {
+        System.out.println("Enter your Last Name");
+        lastName = scanner.next();
+        UserDetailsValidation.isValidLastName(lastName);
     }
 }
