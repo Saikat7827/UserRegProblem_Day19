45d1e928566c72d97634f13bd9bdd3663d31b8e3
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-04 12:30:27 +0530

    Added: Atlest Password should have one number

--- UserDetailsValidation.java
+++ UserDetailsValidation.java
@@ -5,13 +5,13 @@ import java.util.regex.Pattern;
 
 public class UserDetailsValidation {
     public static void isValidPassword(String password) {
-        String regex = "^[A-Z]{1}+[a-zA-z]{7,}$";
+        String regex = "^[A-Z]{1}+[a-z A-z 0-9]{7,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
-            System.out.println("Valid");
+            System.out.println("Valid Password");
         } else {
-            System.out.println("Invalid");
+            System.out.println("Invalid Password");
         }
     }
 }
