8bb55bdc867ace80ed0cc4f3118ec3d487bed9a8
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-04 12:46:03 +0530

    Added One Special Character to Password

--- UserDetailsValidation.java
+++ UserDetailsValidation.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class UserDetailsValidation {
     public static void isValidPassword(String password) {
-        String regex = "^[A-Z]{1}+[a-z A-z 0-9]{7,}$";
+        String regex = "^[A-Z]{1}+[!@#$%^&*()_+]{1}+[a-z A-z 0-9]{7,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
         if (matcher.matches()) {
