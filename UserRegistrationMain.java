be83496d4005c8b5f0fc7008d57e6029bc95bd6e
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-12-06 20:22:04 +0530

    Added : Junit test done for User Details

--- UserRegistrationMain.java
+++ UserRegistrationMain.java
@@ -2,17 +2,6 @@ package com.bridgelabz;
 
 import java.util.Scanner;
 
-public class UserRegistrationMain extends UserDetailsValidation{
-    static Scanner scanner = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Problem");
+public class UserRegistrationMain {
 
-        isValidAllEmail();
-    }
-    public static void userPassword() {
-        System.out.println("Enter your password:");
-
-        String password = scanner.nextLine();
-        UserDetailsValidation.isValidAllEmail();
-    }
 }
